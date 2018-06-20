0e2baa9a675b11cbfbb378d3b16abe6ff12886f8
Jonathan Roitgrund <jroitgrund@palantir.com>
2018-06-20 16:46:57 +0100

    final fixes

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema.define(version: 20140707191403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "details", force: true do |t|
     t.integer "user_id"
     t.string  "site"
